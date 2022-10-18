35c59fd1ae21602ef1e2feba6fab1ecc1d83ce43
Tavi <tavirazin@gmail.com>
2022-10-19 00:56:14 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,25 +6,25 @@ const Navbar = () =>{
         <nav>
             <img className="profile-pic"  src="images/foto2.JPG" alt="foto diri" />
             <div className="nav-links">
-                <Link to="/"><button class="button-49" role="button">Home</button></Link>
+                <Link to="/"><button class="button-49">Home</button></Link>
             </div>
             <div className="nav-links">
-                <Link to="/about"><button class="button-49" role="button">About</button></Link>
+                <Link to="/about"><button class="button-49">About</button></Link>
             </div>
             <div className="nav-links">
-                <Link to="/awards"><button class="button-49" role="button">Awards</button></Link>
+                <Link to="/awards"><button class="button-49">Awards</button></Link>
             </div>
             <div className="nav-links">
-                <Link to="/education"><button class="button-49" role="button">Education</button></Link>
+                <Link to="/education"><button class="button-49">Education</button></Link>
             </div>
             <div className="nav-links">
-                <Link to="/experience"><button class="button-49" role="button">Experience</button></Link>
+                <Link to="/experience"><button class="button-49">Experience</button></Link>
             </div>
             <div className="nav-links">
-                <Link to="/interests"><button class="button-49" role="button">Interests</button></Link>
+                <Link to="/interests"><button class="button-49">Interests</button></Link>
             </div>
             <div className="nav-links">
-                <Link to="/skills"><button class="button-49" role="button">Skills</button></Link>
+                <Link to="/skills"><button class="button-49">Skills</button></Link>
             </div>
         </nav>
     );
